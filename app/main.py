8478d2571adbdb437be700d22403fc4e7189f7fa
Midhun Chakkaravarthy <midhunchakkaravarthybaz@gmail.com>
2026-01-04 13:02:05 +0530

    added cors

--- app/main.py
+++ app/main.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
 from app.api.assessment import router as assessment_router
 from app.api.auth import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(
     title="EDTA Backend",
     version="1.0.0"
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(assessment_router)
 app.include_router(auth_router)
