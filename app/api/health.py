6f7248a2f0c49c6990c4ec70ad45a6c984f7be83
Midhun Chakkaravarthy <midhunchakkaravarthybaz@gmail.com>
2026-03-31 13:34:20 +0530

    editted health.py

--- app/api/health.py
+++ app/api/health.py
@@ -2,10 +2,12 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 from app.db.database import get_db
+from fastapi import Response
+
 
 router = APIRouter()
 
-@router.get("/health/db")
+@router.api_route("/health/db", methods=["GET", "HEAD"])
 def db_health_check(db: Session = Depends(get_db)):
     db.execute(text("SELECT 1"))
-    return {"status": "ok"}
+    return Response(status_code=200)
